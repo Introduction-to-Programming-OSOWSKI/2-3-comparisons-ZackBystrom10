f0e60830670ebb28b0c13c60171094ec8a37235a
ZackBystrom10 <835337@mystma.org>
2022-09-20 09:48:18 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y):
+    if x>y:
+        return "true"
+    else:
+        return "false"
+def lessThan(x,y):
+    if x<y:
+        return "true"
+    else:
+        return "false"
+def equalTo(x,y):
+    if x==y:
+        return "true"
+    else:
+        return "false"
+def greaterOrEqual(x,y):
+    if x==y:
+        return "true"
+    elif x>y:
+        return "true"
+    else:
+        return "false"
+def lessOrEqual(x,y):
+    if x==y:
+        return "true"
+    elif x<y:
+        return "true"
+    else:
+        return "false"
+x=1
+y=1
